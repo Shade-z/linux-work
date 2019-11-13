efd8e0314ea40c0fed078638f07bc2c1ad9ab5e5
Xuanlv <1076160305@qq.com>
2019-11-13 13:24:35 +0800

    week9-revised

--- week9/code/11-4/call.c
+++ week9/code/11-4/call.c
@@ -2,8 +2,9 @@
 #include <unistd.h>
 int main(){
 	printf("call:pid=%d,ppid=%d\n",getpid(),getppid());
-	//system("/home/rlk/linuxprogram/week9/code/test");
-	char *args[]={"/home/rlk/linuxprogram/week9/code/test",NULL};
-	execve("/home/rlk/linuxprogram/week9/code/test",args,NULL);
+	//system("/home/rlk/linuxprogram/week9/code/11-4");
+	char *args[]={"/home/rlk/linuxprogram/week9/code/11-4/test",NULL};
+	execve("/home/rlk/linuxprogram/week9/code/11-4/test",args,NULL);
 	printf("after calling\n");
+	return 0;
 }
--- week9/code/11-4/test3.c
+++ week9/code/11-4/test3.c
@@ -2,28 +2,35 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <wait.h>
 int global1=0;
 int global2;
 int main(){
 	int i=10;
 	static int k=20;
 	int pid=fork();
+	int s;
 	//pid->TASK_STOPPED;
 	if(pid<0){
 		perror("创建子进程失败");
 		exit;
 	}
 	else if(pid==0){
-		printf("child:pid=%d,ppid=%d;address:i=%ls,global1=%ls,global2=%ls\n",pid,getppid(),&i,&global1,&global2);
+		printf("child:pid=%d,ppid=%d;address:i=%ls,global1=%ls,global2=%ls\n",
+			pid,getppid(),&i,&global1,&global2);
 		global1=999;
 		global2=1000;
 		i=12;
 		k=19;
-		sleep(10);
+		sleep(2);
 		printf("child:address:global1=%d,global2=%d;i=%d,k=%d\n",global1,global2,i,k);
-		exit(0);
+		//exit(11);
+		_exit(11);
 	}else{
-	printf("parent:pid=%d,ppid=%d;address:i=%ls,global1=%ls,global2=%ls\n",pid,getppid(),&i,&global1,&global2);
+		wait(&s);
+		printf("parent is runing!exit code =%d\n",WEXITSTATUS(s));
+		printf("parent:pid=%d,ppid=%d;address:i=%ls,global1=%ls,global2=%ls\n",
+			pid,getppid(),&i,&global1,&global2);
 		printf("parent:address:global1=%d,global2=%d;i=%d,k=%d\n",global1,global2,i,k);
 	}
 }
