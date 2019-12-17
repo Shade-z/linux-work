869cd6b62a70e565f94ae9437b116a1e7a4a1ff6
Xuanlv <1076160305@qq.com>
2019-12-17 16:14:18 +0800

    week15

---
+++ week15/code/head.h
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"math.h"
+#include<stdlib.h>
+#include<sys/time.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/param.h>
+#include<unistd.h>
+#include <fcntl.h>
+#include <wait.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <wait.h>
+#include <string.h>
+#include <pthread.h>
+#include <limits.h>
+#include <assert.h>
+#define LOOP 10000000
+#define NUM 4
+#define TN 100
+#define ERITER_FIRST
