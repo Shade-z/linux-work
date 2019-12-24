3ee533975a203dfb9125a758d5d7b2fde818b2c8
Xuanlv <1076160305@qq.com>
2019-12-24 11:53:27 +0800

    week16

---
+++ week16/code/head.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+#include<netdb.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#define SERVPORT 3333
+#define MAXDATASIZE 100//每次最大传输量
